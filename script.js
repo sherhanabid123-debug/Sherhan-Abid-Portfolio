15a4c5cbd12636902de05bc178294df347094cac
Sherhan-a <Sherhanabid123@gmail.com>
2026-01-23 19:22:00 +0530

    Added Lenis smooth scrolling and performance optimizations

--- script.js
+++ script.js
@@ -186,3 +186,22 @@ interactiveElements.forEach(el => {
         cursorOutline.style.borderColor = 'var(--text-accent)';
     });
 });
+
+/* Lenis Smooth Scroll Initialization */
+const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    direction: 'vertical',
+    gestureDirection: 'vertical',
+    smooth: true,
+    mouseMultiplier: 1,
+    smoothTouch: false,
+    touchMultiplier: 2,
+});
+
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
